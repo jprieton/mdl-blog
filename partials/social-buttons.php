271ccfd0f43942926bc798a42a24a2e2a9b9f925
Javier Prieto <jprieton@gmail.com>
2016-08-12 14:59:15 -0400

    First Commit

---
+++ partials/social-buttons.php
@@ -0,0 +1,9 @@
+<a href="#" class="mdl-mini-footer--social-btn social-btn social-btn__twitter">
+  <span class="visuallyhidden">Twitter</span>
+</a>
+<a href="#" class="mdl-mini-footer--social-btn social-btn social-btn__blogger">
+  <span class="visuallyhidden">Facebook</span>
+</a>
+<a href="#" class="mdl-mini-footer--social-btn social-btn social-btn__gplus">
+  <span class="visuallyhidden">Google Plus</span>
+</a>
