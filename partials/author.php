fbd5521099ef1eda7f9269249de2c9eae07c9b19
Javier Prieto <jprieton@gmail.com>
2016-08-15 12:04:12 -0400

    Author partials

--- partials/author.php
+++ partials/author.php
@@ -1,4 +1,4 @@
-<div class="minilogo"></div>
+<div class="minilogo"><?php echo get_avatar( get_the_author_meta( 'ID' ), 44 ); ?></div>
 <div>
   <strong><?php the_author() ?></strong>
   <span><?php printf( __( '%s ago', 'mdl' ), human_time_diff( get_the_time( 'U' ), current_time( 'timestamp' ) ) ) ?></span>
